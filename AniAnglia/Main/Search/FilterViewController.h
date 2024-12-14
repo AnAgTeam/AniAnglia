1912e9335f258d0c6f80e4d6650098b8ea136bcc
Toilet <macosinternal@gmail.com>
2024-12-14 07:27:41 -0800

    Mitigated bugs with MainViewController. Fixed PlayerViewController

---
+++ AniAnglia/Main/Search/FilterViewController.h
@@ -0,0 +1,12 @@
+//
+//  FilterViewController.h
+//  AniAnglia
+//
+//  Created by Toilettrauma on 13.12.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewController : UIViewController
+
+@end
